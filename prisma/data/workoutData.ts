c3534d4560926ccbc61eaf5c473772efb337f8b6
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2024-12-18 15:58:19 -0700

    added db test data and seed.ts file

---
+++ prisma/data/workoutData.ts
@@ -0,0 +1,84 @@
+export const workoutData = {
+    title: 'Full Body Workout A',
+    notes: 'Felt good, performance increased.',
+    date: new Date('2024-10-15'),
+    exercises: [
+        {
+            exerciseId: 93, // (Leg Press)
+            notes: '3 sets of 8-12 reps',
+            weightUnit: 'lbs',
+            sets: [
+                { weight: 385, reps: 12, rpe: 7.5, deleted: false },
+                { weight: 385, reps: 11, rpe: 8.5, deleted: false },
+                { weight: 385, reps: 10, rpe: 9, deleted: false },
+            ],
+            deleted: false
+        },
+        {
+            exerciseId: 71, // (Hamstring Curl (Lying))
+            notes: '3 sets of 8-15 reps',
+            weightUnit: 'lbs',
+            sets: [
+                { weight: 115, reps: 15, rpe: 8, deleted: false },
+                { weight: 115, reps: 11, rpe: 8, deleted: false },
+                { weight: 115, reps: 10, rpe: 10, deleted: false },
+            ],
+            deleted: false
+        },
+        {
+            exerciseId: 111, // (Overhead Press (Dumbell, Seated))
+            notes: '3 sets of 6-10 reps',
+            weightUnit: 'lbs',
+            sets: [
+                { weight: 60, reps: 10, rpe: 9, deleted: false },
+                { weight: 60, reps: 7, rpe: 10, deleted: false },
+                { weight: 60, reps: 6, rpe: 9.5, deleted: false },
+            ],
+            deleted: false
+        },
+        {
+            exerciseId: 83, // (Lat Pulldown (Narrow Grip))
+            notes: '3 sets of 8-12 reps',
+            weightUnit: 'lbs',
+            sets: [
+                { weight: 165, reps: 11, rpe: 9, deleted: false },
+                { weight: 165, reps: 9, rpe: 9.5, deleted: false },
+                { weight: 165, reps: 9, rpe: 10, deleted: false },
+            ],
+            deleted: false
+        },
+        {
+            exerciseId: 17, // (Biceps Curl (Bayesian))
+            notes: '3 sets of 8-15 reps',
+            weightUnit: 'lbs',
+            sets: [
+                { weight: 30, reps: 14, rpe: 9, deleted: false },
+                { weight: 30, reps: 12, rpe: 10, deleted: false },
+                { weight: 30, reps: 10, rpe: 10, deleted: false },
+            ],
+            deleted: false
+        },
+        {
+            exerciseId: 88, // (Lateral Raise (Cable))
+            notes: '3 sets of 10-15 reps',
+            weightUnit: 'lbs',
+            sets: [
+                { weight: 15, reps: 15, rpe: 8, deleted: false },
+                { weight: 15, reps: 14, rpe: 9, deleted: false },
+                { weight: 15, reps: 12, rpe: 10, deleted: false },
+            ],
+            deleted: false
+        },
+        {
+            exerciseId: 178, // (Triceps Extension (Cable, Overhead))
+            notes: '3 sets of 8-15 reps',
+            weightUnit: 'lbs',
+            sets: [
+                { weight: 65, reps: 15, rpe: 7.5, deleted: false },
+                { weight: 65, reps: 11, rpe: 10, deleted: false },
+                { weight: 65, reps: 9, rpe: 10, deleted: false },
+            ],
+            deleted: false
+        },
+    ],
+}
---
+++ prisma/seed.ts
@@ -0,0 +1,96 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+import { exerciseData } from './data/exerciseData';
+import { workoutData } from './data/workoutData';
+
+interface Exercise {
+    name: string
+    id?: number 
+}
+
+interface Set {
+    weight: number
+    reps: number
+    rpe: number
+    deleted: boolean
+}
+
+interface ExerciseInWorkout {
+    exerciseId: number
+    notes: string
+    weightUnit: string
+    sets: Set[]
+}
+
+interface Workout {
+    title: string
+    notes: string
+    date: Date
+    exercises: ExerciseInWorkout[]
+}
+
+async function seedDatabase(exerciseData: Exercise[], workoutData: Workout) {
+
+    const { title, notes, date, exercises } = workoutData;
+
+    for (const exercise of exerciseData) {
+        await prisma.exercise.create({
+            data: {
+                name: exercise.name, 
+            }
+        });
+    }
+
+    await prisma.workout.create({
+        data: {
+            title,
+            notes,
+            date,
+            deleted: false,
+            user: {
+                create: {
+                    email: 'socholotuikt@gmail.com',
+                    firstName: 'Tyler',
+                    lastName: 'Socholotuik',
+                }
+            },
+            exercises: {
+                create: exercises.map((exercise) => ({
+                    exercise: {
+                        connect: { id: exercise.exerciseId }, // Connect an existing exercise by ID
+                    },
+                    notes: exercise.notes,
+                    weightUnit: exercise.weightUnit,
+                    sets: {
+                        create: exercise.sets.map((set) => ({
+                            weight: set.weight,
+                            reps: set.reps,
+                            rpe: set.rpe,
+                            deleted: set.deleted
+                        })),
+                    },
+                    deleted: false
+                })),
+            },
+        },
+        include: {
+            exercises: {
+                include: {
+                    exercise: true,
+                    sets: true,
+                },
+            },
+        },
+    });
+}
+
+seedDatabase(exerciseData as Exercise[], workoutData as Workout)
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
