a84c0859bf844d6e50e0057a684fc2d602e03150
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2024-12-30 13:23:45 -0700

    added weight unit conversion toggle to one rep max

--- pages/calculator.tsx
+++ pages/calculator.tsx
@@ -1,11 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { Button } from "@nextui-org/react";
 
 import CalculatorForm from "@/components/calculator/CalculatorForm";
 
 export default function Calculator() {
     const [setData, setSetData] = useState({});
-    const [oneRepMax, setOneRepMax] = useState(null);
+    const [oneRepMax, setOneRepMax] = useState(0);
     const [rpeTableData, setRPETableData] = useState({});
+    const [conversionWeightUnit, setConversionWeightUnit] = useState("lbs");
+    const { weightUnit } = setData;
+
+    useEffect(() => {
+        // change the conversion unit to the weight unit whenever weight unit changes
+        // so that the conversion calculation is correct for the selected weight unit
+        setConversionWeightUnit(weightUnit);
+    }, [weightUnit]);
+
+    const convertWeightUnit = () => {
+        // toggle the weight conversion unit and update state
+        const conversionUnit = conversionWeightUnit === "lbs" ? "kg" : "lbs";
+        setConversionWeightUnit(conversionUnit);
+
+        // flag to perform unit conversion only when weight unit and conversion
+        // unit are not the same
+        const doUnitConversion = weightUnit !== conversionUnit;
+
+        let conversionRate = 1;
+
+        if (
+            (doUnitConversion && weightUnit === "lbs") ||
+            (!doUnitConversion && weightUnit === "kg")
+        ) {
+            // convet from lbs to kg
+            conversionRate = 0.453592;
+        } else if (
+            (!doUnitConversion && weightUnit === "lbs") ||
+            (doUnitConversion && weightUnit === "kg")
+        ) {
+            // convert from kg to lbs
+            conversionRate = 2.20462;
+        }
+
+        setOneRepMax(Math.round(oneRepMax * conversionRate));
+    };
 
     return (
         <div className="container mx-auto px-2 md:px-4 py-6">
@@ -19,18 +57,45 @@ export default function Calculator() {
                     setRPETableData={setRPETableData}
                 />
                 <div className="text-center">
-                    <p className="text-lg mb-2">
-                        Estimated One-Rep Max:
-                    </p>
-                    {oneRepMax ? 
-                        <p>
-                            {oneRepMax} {setData.weightUnit}
-                        </p> 
-                        : 
+                    <p className="text-lg mb-2">Estimated One-Rep Max:</p>
+                    {oneRepMax > 0 ? (
+                        <>
+                            <p className="font-bold text-xl text-primary mb-4">
+                                {oneRepMax} {conversionWeightUnit}
+                            </p>
+                            <Button
+                                aria-label="weight unit"
+                                color="default"
+                                variant="bordered"
+                                size="sm"
+                                onPress={convertWeightUnit}
+                            >
+                                <span
+                                    className={
+                                        conversionWeightUnit === "lbs"
+                                            ? "text-primary font-bold"
+                                            : ""
+                                    }
+                                >
+                                    lbs
+                                </span>
+                                /
+                                <span
+                                    className={
+                                        conversionWeightUnit === "kg"
+                                            ? "text-primary font-bold"
+                                            : ""
+                                    }
+                                >
+                                    kg
+                                </span>
+                            </Button>
+                        </>
+                    ) : (
                         <p className="italic text-default-500">
                             enter values above to calculate
                         </p>
-                    }
+                    )}
                 </div>
             </section>
         </div>
