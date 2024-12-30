4cd09c49ff86f2bd359f05a9671a0cad5ca1c6d9
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2024-12-30 13:47:42 -0700

    added input validation and clear button to calculator form

--- components/calculator/CalculatorForm.tsx
+++ components/calculator/CalculatorForm.tsx
@@ -12,12 +12,13 @@ export default function CalculatorForm({
     const [weight, setWeight] = useState("");
     const [weightUnit, setWeightUnit] = useState("lbs");
     const [reps, setReps] = useState<string>("");
-    const [rpe, setRPE] = useState(new Set([]));
+    const [rpe, setRPE] = useState(new Set([""]));
 
     const rpeValues = ["6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10"];
 
     useEffect(() => {
         updateOneRepMax(weight, reps, rpe);
+        console.log(rpe.currentKey);
     }, [weight, reps, rpe, weightUnit]);
 
     const toggleWeightUnit = () => {
@@ -29,13 +30,48 @@ export default function CalculatorForm({
         }
     };
 
+    const handleWeightChange = (value: string) => {
+        const parsedValue = parseFloat(value);
+
+        let isValid = false;
+        // rules for weight:
+        // min: 0
+        // max: 9999
+        // must be in steps of 0.5
+        isValid =
+            parsedValue >= 0 && parsedValue <= 9999 && parsedValue % 0.5 === 0;
+
+        // allow input if isValid, otherwise, input is prevented
+        if (isValid) {
+            setWeight(value);
+        }
+    };
+
+    const handleRepsChange = (value: string) => {
+        const parsedValue = parseFloat(value);
+
+        let isValid = false;
+        // rules for reps:
+        // min: 1
+        // max: 10
+        // must be whole number
+        isValid =
+            parsedValue >= 1 &&
+            parsedValue <= 10 &&
+            Number.isInteger(parsedValue);
+
+        if (isValid) {
+            setReps(value);
+        }
+    };
+
     const updateOneRepMax = (weight: string, reps: string, rpe) => {
         const rpeString = rpe.currentKey;
         let weightNumber;
         let repsNumber;
         let rpeNumber;
 
-        if (weight !== "" && reps !== "" && rpe.size > 0) {
+        if (weight !== "" && reps !== "" && rpeString !== undefined) {
             weightNumber = parseFloat(weight);
             repsNumber = parseInt(reps);
             rpeNumber = parseFloat(rpeString);
@@ -57,81 +93,108 @@ export default function CalculatorForm({
         }
     };
 
+    const Clear = () => {
+        setWeight("");
+        setReps("");
+        setWeightUnit("lbs");
+        setRPE(new Set([""]));
+        setSetData({});
+        setOneRepMax(0);
+    }
+
     return (
-        <div className="flex flex-row justify-center items-end gap-4 mb-6">
-            <div>
-                <Input
-                    className="w-[90px]"
-                    id="weight"
-                    label="Weight"
-                    labelPlacement="outside"
-                    variant="bordered"
-                    size="sm"
-                    type="number"
-                    endContent={
-                        <div className="pointer-events-none flex items-center">
-                            <span className="text-default-400 text-small">
-                                {weightUnit}
-                            </span>
-                        </div>
-                    }
-                    value={weight}
-                    onValueChange={setWeight}
-                />
+        <div>
+            <div className="flex flex-row justify-center items-end gap-4 mb-2">
+                <div>
+                    <Input
+                        className="w-[90px]"
+                        id="weight"
+                        label="Weight"
+                        labelPlacement="outside"
+                        variant="bordered"
+                        size="sm"
+                        type="number"
+                        min={0}
+                        max={9999}
+                        step={0.5}
+                        endContent={
+                            <div className="pointer-events-none flex items-center">
+                                <span className="text-default-400 text-small">
+                                    {weightUnit}
+                                </span>
+                            </div>
+                        }
+                        value={weight}
+                        onValueChange={handleWeightChange}
+                    />
+                </div>
+                <div className="">
+                    <Button
+                        aria-label="weight unit"
+                        color="default"
+                        variant="bordered"
+                        size="sm"
+                        onPress={toggleWeightUnit}
+                    >
+                        <span
+                            className={
+                                weightUnit === "lbs" ? "text-primary font-bold" : ""
+                            }
+                        >
+                            lbs
+                        </span>
+                        /
+                        <span
+                            className={
+                                weightUnit === "kg" ? "text-primary font-bold" : ""
+                            }
+                        >
+                            kg
+                        </span>
+                    </Button>
+                </div>
+                <div>
+                    <Input
+                        className="w-[60px]"
+                        id="reps"
+                        label="Reps"
+                        labelPlacement="outside"
+                        variant="bordered"
+                        size="sm"
+                        type="number"
+                        min={1}
+                        max={10}
+                        step={1}
+                        value={reps}
+                        onValueChange={handleRepsChange}
+                    />
+                </div>
+                <div>
+                    <Select
+                        className="w-[80px]"
+                        id="rpe"
+                        label="RPE"
+                        labelPlacement="outside"
+                        variant="bordered"
+                        size="sm"
+                        selectedKeys={rpe}
+                        onSelectionChange={setRPE}
+                    >
+                        {rpeValues.map((value) => (
+                            <SelectItem key={value}>{value}</SelectItem>
+                        ))}
+                    </Select>
+                </div>
             </div>
-            <div className="">
+            <div className="justify-self-center">
                 <Button
-                    aria-label="weight unit"
-                    color="default"
-                    variant="bordered"
-                    size="sm"
-                    onPress={toggleWeightUnit}
+                    color="danger"
+                    variant="light"
+                    size="md"
+                    onPress={Clear}
                 >
-                    <span
-                        className={
-                            weightUnit === "lbs" ? "text-primary font-bold" : ""
-                        }
-                    >
-                        lbs
-                    </span>
-                    /
-                    <span
-                        className={
-                            weightUnit === "kg" ? "text-primary font-bold" : ""
-                        }
-                    >
-                        kg
-                    </span>
+                    Clear
                 </Button>
-            </div>
-            <div>
-                <Input
-                    className="w-[60px]"
-                    id="reps"
-                    label="Reps"
-                    labelPlacement="outside"
-                    variant="bordered"
-                    size="sm"
-                    type="number"
-                    value={reps}
-                    onValueChange={setReps}
-                />
-            </div>
-            <div>
-                <Select
-                    className="w-[80px]"
-                    id="rpe"
-                    label="RPE"
-                    labelPlacement="outside"
-                    variant="bordered"
-                    size="sm"
-                    selectedKeys={rpe}
-                    onSelectionChange={setRPE}
-                >
-                    {rpeValues.map((value) => (
-                        <SelectItem key={value}>{value}</SelectItem>
-                    ))}
-                </Select>
             </div>
         </div>
     );
