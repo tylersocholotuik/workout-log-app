72f592368148c39544a9b6929c8e3e7525161c1d
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2025-01-04 15:36:52 -0700

    created welcome modal on login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,141 +1,38 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-import NavBar from "@/components/NavBar";
+import { supabase } from "@/utils/supabase/supabaseClient";
 
-import {
-  addUserExercise,
-  updateUserExercise,
-  deleteUserExercise,
-} from "@/utils/api/exercises";
-import {
-  addWorkout,
-  updateWorkout,
-  getWorkout,
-  deleteWorkout,
-} from "@/utils/api/workouts";
-import { Workout } from "@/utils/models/models";
+import { useAuth } from "@/components/auth/AuthProvider";
 
-import { workoutData } from "@/prisma/data/workoutData2";
-import { workoutDataUpdate } from "@/prisma/data/workoutData2Update";
+import { useDisclosure } from "@nextui-org/react";
+
+import LoginModal from "@/components/LoginModal";
 
 export default function Home() {
-  const [exerciseName, setExerciseName] = useState("");
-  const [error, setError] = useState<string | null>(null);
+  
+  const { user } = useAuth();
 
-  const userId = "cm4zr6sti0000priwve9whn6t"; // test user
-  const workoutId = 2;
-  const exerciseId = 2;
+  const loginModal = useDisclosure();
 
+  // listening for the user sign in event to display the welcome modal
   useEffect(() => {
-    // updateExistingWorkout(userId, workoutDataUpdate)
-    // addNewWorkout(userId, workoutData)
-    // loadWorkout(userId, workoutId)
-    // softDeleteWorkout(userId, workoutId)
-  }, []);
-
-  const loadWorkout = async (userId: string, id: number) => {
-    const data = await getWorkout(userId, id);
-    console.log(data);
-  };
-
-  const softDeleteWorkout = async (userId: string, workoutId: number) => {
-    setError(null);
-    try {
-      const data = await deleteWorkout(userId, workoutId);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    }
-  };
-
-  const addNewExercise = async (userId: string, name: string) => {
-    setError(null);
-    try {
-      const data = await addUserExercise(userId, name);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    }
-  };
-
-  const updateExercise = async (
-    userId: string,
-    exerciseId: number,
-    name: string
-  ) => {
-    setError(null);
-    try {
-      const data = await updateUserExercise(userId, exerciseId, name);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    }
-  };
-
-  const deleteExercise = async (userId: string, exerciseId: number) => {
-    setError(null);
-    try {
-      const data = await deleteUserExercise(userId, exerciseId);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    }
-  };
-
-  const addNewWorkout = async (userId: string, workoutData: Workout) => {
-    setError(null);
-    try {
-      const data = await addWorkout(userId, workoutData);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    }
-  };
-
-  const updateExistingWorkout = async (
-    userId: string,
-    workoutData: Workout
-  ) => {
-    setError(null);
-    try {
-      const data = await updateWorkout(userId, workoutData);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-      if (error instanceof Error) {
-        setError(error.message);
-      }
-    }
-  };
-
-  const handleAddExercise = async () => {
-    addNewExercise(userId, exerciseName);
-  };
-
-  const handleUpdateExercise = async () => {
-    updateExercise(userId, exerciseId, exerciseName);
-  };
-
-  const handleDeleteExercise = async () => {
-    deleteExercise(userId, exerciseId);
-  };
+    const { data } = supabase.auth.onAuthStateChange(
+        (event) => {
+            if (event === "INITIAL_SESSION") {
+                loginModal.onOpen();
+            } if (event === "SIGNED_IN") {
+                loginModal.onOpen();
+            }
+        }
+    );
+}, []);
 
   return <>
+    <LoginModal 
+      isOpen={loginModal.isOpen}
+      onOpenChange={loginModal.onOpenChange}
+      displayName={user?.user_metadata.display_name ?? user?.email}
+    />
   </>
 }
