ac04907a8cf079d0b770d6ff09b3e5a66c3e6cf9
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2025-01-04 17:18:35 -0700

    got rid of login modal. Triggered every time home page loads

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,7 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-
-import { supabase } from "@/utils/supabase/supabaseClient";
-
-import { useAuth } from "@/components/auth/AuthProvider";
-
-import { useDisclosure } from "@nextui-org/react";
-
-import LoginModal from "@/components/LoginModal";
-
 export default function Home() {
-  
-  const { user } = useAuth();
 
-  const loginModal = useDisclosure();
-
-  // listening for the user sign in event to display the welcome modal
-  useEffect(() => {
-    const { data } = supabase.auth.onAuthStateChange(
-        (event) => {
-            if (event === "INITIAL_SESSION") {
-                loginModal.onOpen();
-            } if (event === "SIGNED_IN") {
-                loginModal.onOpen();
-            }
-        }
-    );
-}, []);
 
   return <>
-    <LoginModal 
-      isOpen={loginModal.isOpen}
-      onOpenChange={loginModal.onOpenChange}
-      displayName={user?.user_metadata.display_name ?? user?.email}
-    />
+
   </>
 }
