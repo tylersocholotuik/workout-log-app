1dc149028fe1e2e3c17fe48acc2715ab033af4c1
TylerSocholotuik <143831033+TylerSocholotuik@users.noreply.github.com>
2025-01-24 17:32:33 -0700

    removed workout from seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,85 +1,24 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 import { exerciseData } from './data/exerciseData';
-import { workoutData } from './data/workoutData';
 
 interface Exercise {
-    name: string
-    id?: number 
+    name: string;
+    id?: number;
 }
 
-interface Set {
-    weight: number
-    reps: number
-    rpe: number
-    deleted: boolean
-}
-
-interface ExerciseInWorkout {
-    exerciseId: number
-    notes: string
-    weightUnit: string
-    sets: Set[]
-}
-
-interface Workout {
-    title: string
-    notes: string
-    date: Date
-    exercises: ExerciseInWorkout[]
-}
-
-async function seedDatabase(exerciseData: Exercise[], workoutData: Workout) {
-
-    const { title, notes, date, exercises } = workoutData;
-
+async function seedDatabase(exerciseData: Exercise[]) {
     for (const exercise of exerciseData) {
         await prisma.exercise.create({
             data: {
-                name: exercise.name, 
-            }
+                name: exercise.name,
+            },
         });
     }
-
-    await prisma.workout.create({
-        data: {
-            title,
-            notes,
-            date,
-            deleted: false,
-            userId: "b24993de-ff97-4547-87d0-9997638c319b",
-            exercises: {
-                create: exercises.map((exercise) => ({
-                    exercise: {
-                        connect: { id: exercise.exerciseId }, // Connect an existing exercise by ID
-                    },
-                    notes: exercise.notes,
-                    weightUnit: exercise.weightUnit,
-                    sets: {
-                        create: exercise.sets.map((set) => ({
-                            weight: set.weight,
-                            reps: set.reps,
-                            rpe: set.rpe,
-                            deleted: set.deleted
-                        })),
-                    },
-                    deleted: false
-                })),
-            },
-        },
-        include: {
-            exercises: {
-                include: {
-                    exercise: true,
-                    sets: true,
-                },
-            },
-        },
-    });
 }
 
-seedDatabase(exerciseData as Exercise[], workoutData as Workout)
+seedDatabase(exerciseData as Exercise[])
     .then(async () => {
         await prisma.$disconnect();
     })
@@ -87,4 +26,4 @@ seedDatabase(exerciseData as Exercise[], workoutData as Workout)
         console.error(e);
         await prisma.$disconnect();
         process.exit(1);
-    })
+    });
