2dfbba8d22994868af196f23756c9f6f9aa414c4
Tyler Socholotuik <socholotuikt@gmail.com>
2025-05-17 10:10:06 -0600

    changed toast placement to bottom-center

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <HeroUIProvider>
-      <ToastProvider />
+      <ToastProvider placement="bottom-center" />
       <NextThemesProvider attribute="class" defaultTheme="dark">
         <AuthProvider>
           <NavBar />
